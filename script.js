35c28627ebc03a101837f17b7784de09db047245
kaifi477 <145561737+kaifi477@users.noreply.github.com>
2023-09-26 00:44:57 +0530

    index.html with resources

---
+++ script.js
@@ -0,0 +1,117 @@
+let categoryList = document.querySelector(".category-list");
+let categoryList2 = document.querySelector(".category-list-2");
+let categoryList3 = document.querySelector(".category-list-3");
+let categoryList4 = document.querySelector(".category-list-4");
+let categoryList5 = document.querySelector(".category-list-5");
+let categoryList6 = document.querySelector(".category-list-6");
+let categoryList7 = document.querySelector(".category-list-7");
+let categoryList8 = document.querySelector(".category-list-8");
+let categoryList9 = document.querySelector(".category-list-9");
+let categoryList10 = document.querySelector(".category-list-10");
+
+
+let login_modal =document.querySelector('#login-modal-open')
+
+// *category menu
+
+// Define the functions
+function categoryListOver() {
+    categoryList.style.display = "flex";
+    login_modal.style.display='none'
+
+  }
+  
+  function categoryListOut() {
+    categoryList.style.display = "";
+    login_modal.style.display='flex'
+
+  }
+  
+  function categoryList2Over() {
+    categoryList2.style.display = "flex";
+    login_modal.style.display='none'
+
+  }
+  
+  function categoryList2Out() {
+    categoryList2.style.display = "";
+    login_modal.style.display='flex'
+  }
+  function categoryList3Over() {
+    categoryList3.style.display = "flex";
+    login_modal.style.display='none'
+
+  }
+  
+  function categoryList3Out() {
+    login_modal.style.display='flex'
+    categoryList3.style.display = "";
+  }
+  function categoryList4Over() {
+    login_modal.style.display='none'
+    categoryList4.style.display = "flex";
+  }
+  
+  function categoryList4Out() {
+    login_modal.style.display='flex'
+    categoryList4.style.display = "";
+  }
+  function categoryList5Over() {
+    login_modal.style.display='none'
+    categoryList5.style.display = "flex";
+  }
+  
+  function categoryList5Out() {
+    login_modal.style.display='flex'
+    categoryList5.style.display = "";
+  }
+  function categoryList6Over() {
+    login_modal.style.display='none'
+    categoryList6.style.display = "flex";
+  }
+  
+  function categoryList6Out() {
+    login_modal.style.display='flex'
+    categoryList6.style.display = "";
+  }
+  
+  function categoryList7Over() {
+    login_modal.style.display='none'
+    categoryList7.style.display = "flex";
+  }
+  function categoryList7Out() {
+    login_modal.style.display='flex'
+    categoryList7.style.display = "";
+  }
+  function categoryList8Over() {
+    login_modal.style.display='none'
+    categoryList8.style.display = "flex";
+  }
+  
+  function categoryList8Out() {
+    login_modal.style.display='flex'
+    categoryList8.style.display = "";
+  }
+  
+  function categoryList9Over() {
+    login_modal.style.display='none'
+    categoryList9.style.display = "flex";
+  }
+  function categoryList9Out() {
+    login_modal.style.display='flex'
+    categoryList9.style.display = "";
+  }
+  function categoryList10Over() {
+    login_modal.style.display='none'
+    categoryList10.style.display = "flex";
+  }
+  
+  function categoryList10Out() {
+    login_modal.style.display='flex'
+    categoryList10.style.display = "";
+  }
+
+  // option menu
+
+  // //option menu
+  
