0a02eb4e48f1324919383ca80213fd44374d8727
mohd kaif <145561737+kaifi477@users.noreply.github.com>
2024-05-05 16:23:24 +0530

    Update script.js
    updated

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let categoryList7 = document.querySelector(".category-list-7");
 let categoryList8 = document.querySelector(".category-list-8");
 let categoryList9 = document.querySelector(".category-list-9");
 let categoryList10 = document.querySelector(".category-list-10");
+let flash_sale = document.querySelector(".flash-sale");
 
 
 let login_modal =document.querySelector('#login-modal-open')
@@ -18,100 +19,156 @@ let login_modal =document.querySelector('#login-modal-open')
 function categoryListOver() {
     categoryList.style.display = "flex";
     login_modal.style.display='none'
-
+    $('.blog').css('display', 'none');
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
   }
   
   function categoryListOut() {
     categoryList.style.display = "";
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
+    $('.flash-sale').css('display', 'block');
 
   }
   
   function categoryList2Over() {
     categoryList2.style.display = "flex";
     login_modal.style.display='none'
+    $('.blog').css('display', 'none');
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
 
   }
   
   function categoryList2Out() {
     categoryList2.style.display = "";
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
+    $('.flash-sale').css('display', 'block');
+
   }
   function categoryList3Over() {
     categoryList3.style.display = "flex";
     login_modal.style.display='none'
+    $('.blog').css('display', 'none');
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
 
   }
   
   function categoryList3Out() {
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
     categoryList3.style.display = "";
+    $('.flash-sale').css('display', 'block');
   }
   function categoryList4Over() {
     login_modal.style.display='none'
+    $('.blog').css('display', 'none');
     categoryList4.style.display = "flex";
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
+
   }
   
   function categoryList4Out() {
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
+    $('.flash-sale').css('display', 'block');
+
     categoryList4.style.display = "";
   }
   function categoryList5Over() {
     login_modal.style.display='none'
+    $('.blog').css('display', 'none');
     categoryList5.style.display = "flex";
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
+
   }
   
   function categoryList5Out() {
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
+    $('.flash-sale').css('display', 'block');
+
     categoryList5.style.display = "";
   }
   function categoryList6Over() {
     login_modal.style.display='none'
+    $('.blog').css('display', 'none');
     categoryList6.style.display = "flex";
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
+
   }
   
   function categoryList6Out() {
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
+    $('.flash-sale').css('display', 'block');
+
     categoryList6.style.display = "";
   }
   
   function categoryList7Over() {
     login_modal.style.display='none'
+    $('.blog').css('display', 'none');
     categoryList7.style.display = "flex";
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
+
   }
   function categoryList7Out() {
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
+    $('.flash-sale').css('display', 'block');
+
     categoryList7.style.display = "";
   }
   function categoryList8Over() {
     login_modal.style.display='none'
+    $('.blog').css('display', 'none');
     categoryList8.style.display = "flex";
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
+
   }
   
   function categoryList8Out() {
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
+    $('.flash-sale').css('display', 'block');
+
     categoryList8.style.display = "";
   }
   
   function categoryList9Over() {
     login_modal.style.display='none'
-    categoryList9.style.display = "flex";
+    $('.blog').css('display', 'none');
+    // categoryList9.style.display = "flex";
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
+
   }
   function categoryList9Out() {
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
+    $('.flash-sale').css('display', 'block');
+
     categoryList9.style.display = "";
   }
   function categoryList10Over() {
     login_modal.style.display='none'
+    $('.blog').css('display', 'none');
     categoryList10.style.display = "flex";
+    $('.flash-sale').css('display', 'none');
+    $('.all-brands').css('display', 'none');
+
   }
   
   function categoryList10Out() {
-    login_modal.style.display='flex'
+    // login_modal.style.display='flex'
+    $('.flash-sale').css('display', 'block');
+
     categoryList10.style.display = "";
+    // $('.flash-sale').css('display', 'block');
+
   }
 
   // option menu
 
   // //option menu
-  
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
